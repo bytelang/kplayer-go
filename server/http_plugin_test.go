622331a7a5a7bb94998121e4afee1da070f7d610
karaler <karaler@bytelang.com>
2022-06-27 10:51:58 +0800

    feat: add plugin testcase

--- server/http_plugin_test.go
+++ server/http_plugin_test.go
@@ -55,3 +55,91 @@ func TestPluginAdd(t *testing.T) {
 
 	t.Log(string(body))
 }
+
+func TestPluginUpdate(t *testing.T) {
+	postData := struct {
+		Unique string            `json:"unique"`
+		Params map[string]string `json:"params"`
+	}{
+		"plugin-1",
+		map[string]string{
+			"fontcolor": "red",
+		},
+	}
+	postDataBytes, _ := json.Marshal(postData)
+	req, err := http.NewRequest("PATCH", Host+"plugin/update", bytes.NewBuffer(postDataBytes))
+	assertError(t, err)
+
+	resp, err := getClient().Do(req)
+	assertError(t, err)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	assertError(t, err)
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal(string(body))
+	}
+
+	t.Log(string(body))
+}
+
+func TestPluginInvalidUpdate(t *testing.T) {
+	postData := struct {
+		Unique string            `json:"unique"`
+		Params map[string]string `json:"params"`
+	}{
+		"invalid-1",
+		map[string]string{
+			"fontcolor": "red",
+		},
+	}
+	postDataBytes, _ := json.Marshal(postData)
+	req, err := http.NewRequest("PATCH", Host+"plugin/update", bytes.NewBuffer(postDataBytes))
+	assertError(t, err)
+
+	resp, err := getClient().Do(req)
+	assertError(t, err)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	assertError(t, err)
+
+	if resp.StatusCode == http.StatusOK {
+		t.Fatal(string(body))
+	}
+
+	t.Log(string(body))
+}
+
+func TestPluginRemove(t *testing.T) {
+	req, err := http.NewRequest("DELETE", Host+"plugin/remove/plugin-1", nil)
+	assertError(t, err)
+
+	resp, err := getClient().Do(req)
+	assertError(t, err)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	assertError(t, err)
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal(string(body))
+	}
+
+	t.Log(string(body))
+}
+
+func TestPluginRemoveInvalid(t *testing.T) {
+	req, err := http.NewRequest("DELETE", Host+"plugin/remove/plugin-1", nil)
+	assertError(t, err)
+
+	resp, err := getClient().Do(req)
+	assertError(t, err)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	assertError(t, err)
+
+	if resp.StatusCode == http.StatusOK {
+		t.Fatal(string(body))
+	}
+
+	t.Log(string(body))
+}
